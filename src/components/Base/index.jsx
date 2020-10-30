469532e896cdbb32439fd50e908f3f0765b8da26
fcq <chaoqun.feng@raykite.com>
2020-10-30 17:16:13 +0800

    [add components] add Base

---
+++ src/components/Base/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * author: fc1904
+ * date: 2020/10/30 16:28:53
+ * fileName: index.jsx
+ * description: 基础布局组件
+ **/
+
+import React from 'react';
+import styles from './index.less';
+
+export default function Base(props) {
+  return <div>1111</div>;
+}
---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+/**
+ * author: fc1904
+ * date: 2020/10/30 16:32:39
+ * fileName: index.jsx
+ * description: components 文件夹 组件输出文件
+ **/
+
+import Base from './Base';
+
+export default {
+  Base, // 主页
+};
